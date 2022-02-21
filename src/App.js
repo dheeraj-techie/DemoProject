106a107f64c318dda5348574c445291a77892d11
Dheeraj Anchan <dheeraj.anchan@gmail.com>
2022-02-21 17:35:33 +0530

    commit new changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Qualys
+          Learn Qualys by doing things
         </a>
       </header>
     </div>
